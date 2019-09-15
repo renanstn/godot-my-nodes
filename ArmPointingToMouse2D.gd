27147749957a4305cebfb092d5df475635d33dac
Renan Santana Desiderio <renan_ota@hotmail.com>
2019-09-14 22:35:24 -0300

    Update ArmPointingToMouse2D.gd

--- ArmPointingToMouse2D.gd
+++ ArmPointingToMouse2D.gd
@@ -1,14 +1,21 @@
+tool
 extends Node2D
 
 class_name ArmPointingToMouse2D
 
+export var FIX_ROTATION : bool
+
 func _process(delta):
 	look_to_mouse()
+	if FIX_ROTATION:
+		fix_rotation()
 
 
 func look_to_mouse() -> void:
 	look_at(get_global_mouse_position())
-	
+
+
+func fix_rotation():
 	if global_position.x < get_global_mouse_position().x:
 		set_scale(Vector2(1, 1))
 	else:
